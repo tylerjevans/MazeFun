564523497bc03d775d6ecc68146db07a30350544
CrashDanger <tylerjevans@gmail.com>
2017-02-15 10:53:35 -0800

    switched whole way, loading resources isnt working

--- MazeFun/Forms/MainMenu.cs
+++ MazeFun/Forms/MainMenu.cs
@@ -43,7 +43,7 @@ namespace MazeFun
             dialog.Filter += "maze format | *.png";
             if (dialog.ShowDialog() == DialogResult.OK)
             {
-                
+                LoadTiles();
                 MazePower TestPower = new MazePower();
                 var test = TestPower.makeMazeTiles(Convert.ToInt32(WidthValue.Value), Convert.ToInt32(HeightValue.Value));
                 Bitmap Maze = imagetoMaze(test);
@@ -95,126 +95,44 @@ namespace MazeFun
                 {
                     if (Tile.right)
                     {
-                        if (Tile.down) return new Bitmap(Properties.Resources.Simple4way); // left, up, right, down
-                        else
-                        {
-                            Bitmap tileresult = new Bitmap(Properties.Resources._3way);
-                            tileresult.RotateFlip(RotateFlipType.Rotate180FlipNone);
-                            return tileresult; //left, up, right
-                        }
+                        if (Tile.down) return cross; // left, up, right, down
+                        return _3way180; //left, up, right
                     }
-                    else
-                    {
-                        if (Tile.down)
-                        {
-                            Bitmap tileresult = new Bitmap(Properties.Resources._3way);
-                            tileresult.RotateFlip(RotateFlipType.Rotate90FlipNone);
-                            return tileresult; // left, up, down
-                        }
-                        else
-                        {
-                            Bitmap tileresult = new Bitmap(Properties.Resources.Simplecorner);
-                            tileresult.RotateFlip(RotateFlipType.Rotate270FlipNone);
-                            return tileresult; // left, up
-                        }
-                    }
+                    if (Tile.down) return _3way90; // left, up, down
+                    return corner270; // left, up
                 }
-                else
+                if (Tile.right)
                 {
-                    if (Tile.right)
-                    {
-                        if (Tile.down)
-                        {
-                            return new Bitmap(Properties.Resources._3way); // left, right, down
-                        }
-                        else
-                        {
-                            return new Bitmap(Properties.Resources.Simplestraight); // left, right
-                        }
-                    }
-                    if (Tile.down)
-                    {
-                        Bitmap tileresult = new Bitmap(Properties.Resources.Simplecorner);
-                        tileresult.RotateFlip(RotateFlipType.Rotate180FlipNone);
-                        return tileresult; //left, down
-                    }
-                    else
-                    {
-                        Bitmap tileresult = new Bitmap(Properties.Resources.Simpleend);
-                        tileresult.RotateFlip(RotateFlipType.Rotate180FlipNone);
-                        return tileresult; // left
-                    }
+                    if (Tile.down) return _3way; // left, right, down
+                    return straight; // left, right
                 }
+                if (Tile.down) return corner180; //left, down
+                return end180; // left
             }
             if (Tile.up)
             {
                 if (Tile.down)
                 {
-                    if (Tile.right)
-                    {
-                        Bitmap tileresult = new Bitmap(Properties.Resources._3way);
-                        tileresult.RotateFlip(RotateFlipType.Rotate270FlipNone);
-                        return tileresult; // up, down, right
-                    }
-                    else
-                    {
-                        Bitmap tileresult = new Bitmap(Properties.Resources.Simplestraight);
-                        tileresult.RotateFlip(RotateFlipType.Rotate90FlipNone);
-                        return tileresult; // up, down
-                    }
-                } else {
-                    if (Tile.right)
-                    {
-                        return new Bitmap(Properties.Resources.Simplecorner); //up,right
-                    }
-                    else
-                    {
-                        Bitmap tileresult = new Bitmap(Properties.Resources.Simpleend);
-                        tileresult.RotateFlip(RotateFlipType.Rotate270FlipNone);
-                        return tileresult; // up
-                    } }
+                    if (Tile.right) return _3way270; // up, down, right
+                    return straight90; // up, down
+                }
+                if (Tile.right) return corner; //up,right
+                return end270; // up
             }
             if (Tile.down)
             {
-                if (Tile.right)
-                {
-                    Bitmap tileresult = new Bitmap(Properties.Resources.Simplecorner);
-                    tileresult.RotateFlip(RotateFlipType.Rotate90FlipNone);
-                    return tileresult; //down,right
-                }
-                else {
-                    Bitmap tileresult = new Bitmap(Properties.Resources.Simpleend);
-                    tileresult.RotateFlip(RotateFlipType.Rotate90FlipNone);
-                    return tileresult; // down
-                }
+                if (Tile.right)return corner90; //down,right
+                return end90; // down
             }
             // if (Tile.right) {}
-            return new Bitmap(Properties.Resources.Simpleend); // wrong default for testing
+            return end; // wrong default for testing
         }
 
         public Bitmap imagetoMaze(mazeTile[,] mazeImage)
         {
             int width = mazeImage.GetLength(0);
             int height = mazeImage.GetLength(1);
-            Bitmap[,] mazeDisplay = new Bitmap[width,height]; // maze display needs to be converted to ref list with rotation counter, save memory
-            for (int y = 0; y < height; y++)
-            {
-                for (int x = 0; x < width; x++)
-                {
-                    if (x == 0 & y == 0) mazeDisplay[0, 0] = new Bitmap(Properties.Resources.Simplestart);
-                    else if (x == width - 1 & y == height - 1)
-                    {
-                        var temp = new Bitmap(Properties.Resources.Simplefinish);
-                        if (mazeImage[x, y].left) temp.RotateFlip(RotateFlipType.RotateNoneFlipX);
-                        else temp.RotateFlip(RotateFlipType.Rotate270FlipX);
-                        mazeDisplay[x, y] = temp;
-                    }
-                        
-                    else
-                        mazeDisplay[x, y] = TileToImage(mazeImage[x, y]);
-                }
-            }
-            int offset = mazeDisplay[0, 0].Width; // assuming same with all tiles and square
+            int offset = cross.Width; // assuming same with all tiles and square
             var result = new Bitmap(offset * width, offset * height);
             using (var canvas = Graphics.FromImage(result))
             {
@@ -222,7 +140,13 @@ namespace MazeFun
                 {
                     for (int x = 0; x < width; x++)
                     {
-                        canvas.DrawImage(mazeDisplay[x,y], new Rectangle(x * offset, y * offset, offset, offset));
+                        if (x == 0 & y == 0) canvas.DrawImage(start, new Rectangle(x * offset, y * offset, offset, offset));
+                        else if (x == width - 1 & y == height - 1)
+                        {
+                            if (mazeImage[x, y].left) finish.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                            else finish.RotateFlip(RotateFlipType.Rotate270FlipX);
+                            canvas.DrawImage(finish, new Rectangle(x * offset, y * offset, offset, offset));
+                        } else canvas.DrawImage(TileToImage(mazeImage[x,y]), new Rectangle(x * offset, y * offset, offset, offset));
                     }
                 }
             }
